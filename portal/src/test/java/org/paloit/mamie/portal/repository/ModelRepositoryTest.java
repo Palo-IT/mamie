33f3637279b14e49b6882bbe5e786be307934d9b
Loudsi <louismaistre@gmail.com>
2020-07-15 15:05:21 +0200

    fix imports

--- portal/src/test/java/org/paloit/mamie/portal/repository/ModelRepositoryTest.java
+++ portal/src/test/java/org/paloit/mamie/portal/repository/ModelRepositoryTest.java
@@ -8,7 +8,6 @@ import org.paloit.mamie.portal.model.Organisation;
 import org.paloit.mamie.portal.model.Project;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.Collections;
 import java.util.List;
